d617befed3fb7dc7758cf224638dde1d556fac05
Yvon Meunier <meunieryvon6@gmail.com>
2022-09-10 20:57:30 -0400

    Update game.js
    Need to  fix bugs and add animation

--- javascript/game.js
+++ javascript/game.js
@@ -1,8 +1,6 @@
 import { pickAWord, isWordInList } from "./words.js";
 import { generateGrid } from "./grid.js";
 
-// TODO: Color change
-
 let currentWord;
 let currentLetterIndex;
 let currentRow;
@@ -33,7 +31,6 @@ function handleInput(e) {
 
     if (text != " " && text != "Enter" && String.fromCharCode(e.keyCode).match(/(\w|\s)/g) && !isFinite(e.key)) {
         letter_elem.textContent = text.toUpperCase();
-        // set typed word letter to the letter typed according to the index
         typedWord = typedWord.substring(0, currentLetterIndex) + text.toUpperCase() + typedWord.substring(currentLetterIndex + 1);
         if (currentLetterIndex != (currentWord.length * 6) - 1 && currentLetterIndex != ((currentRow + 1) * currentWord.length) - 1) {
             currentLetterIndex++;
@@ -89,8 +86,8 @@ export function submitInput() {
         for (let i = 0; i < typedWord.length; i++) {
             let letter_elem = document.querySelector(`[block-${i + (currentRow * currentWord.length)}]`);
             let kb_letter_elem = document.querySelector(`[value=${letter_elem.textContent}]`);
-            if (currentWord.includes(typedWord[i])) {
-                if (letter_elem.textContent == typedWord[i]) {
+            if (currentWord.includes(typedWord[i].toLowerCase())) {
+                if (letter_elem.textContent.toLowerCase() == typedWord[i].toLowerCase()) {
                     letter_elem.style.backgroundColor = "green";
                     kb_letter_elem.style.backgroundColor = "green";
                 } else {
@@ -104,12 +101,18 @@ export function submitInput() {
 
         }
         currentRow++;
-        // log typed word and current word
-        console.log(typedWord);
-        console.log(currentWord);
-        if (typedWord == currentWord) {
+        if (typedWord.toLowerCase() == currentWord) {
             // WIN THEN SETUP()
             console.log("WIN");
+            // reset styles
+            for (let i = 0; i < currentWord.length; i++) {
+                document.querySelector(`[block-${i + (currentRow * currentWord.length)}]`).style.backgroundColor = "";
+            }
+            // reset keyboard styles
+            for (let i = 0; i < 26; i++) {
+                document.querySelector(`[value=${String.fromCharCode(65 + i)}]`).style.backgroundColor = "";
+            }
+
             setup();
         }
         // if  currentRow == 6
@@ -120,4 +123,5 @@ export function submitInput() {
     } else {
         clearTypedWord();
     }
+    typedWord = "";
 }
